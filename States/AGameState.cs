ed8bd5810e39edc5f2e0a8e0c16dca6f988fda1b
goshikun <patanthony2091@gmail.com>
2012-01-28 14:44:52 -0500

    states

---
+++ States/AGameState.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rain.States
+{
+    // Abstract version of a game state
+    abstract public class AGameState
+    {
+        AGameState m_PrevState;
+        internal AGameState PrevState
+        {
+            get { return m_PrevState; }
+            set { m_PrevState = value; }
+        }
+        
+        Boolean m_bUpdate;
+        public Boolean BUpdate
+        {
+            get { return m_bUpdate; }
+            set { m_bUpdate = value; }
+        }
+        
+        Boolean m_bRender;
+        public Boolean BRender
+        {
+            get { return m_bRender; }
+            set { m_bRender = value; }
+        }
+
+        Boolean m_bInitialized;
+        public Boolean BInitialized
+        {
+            get { return m_bInitialized; }
+        }
+
+        public AGameState()
+        {
+            m_PrevState = null;
+            m_bUpdate = false;
+            m_bRender = true;
+            m_bInitialized = true;
+        }
+
+        abstract public void Enter();
+        abstract public void Input();
+        abstract public void Update(float fDTime);
+        abstract public void Render();
+        abstract public void Exit();
+    }
+}
---
+++ States/CMainMenuState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rain.States
+{
+    public class CMainMenuState : AGameState
+    {
+        override public void Enter()
+        {
+        }
+        override public void Input()
+        {
+        }
+        override public void Update(float fDTime) { }
+        override public void Render() { }
+        override public void Exit() { }
+    }
+}
